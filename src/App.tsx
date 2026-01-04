858658dacd1dfda24a61313f9f707e290783b03d
Rachana Priya Martha <martharachanapriya@gmail.com>
2026-01-05 03:04:28 +0530

    Initial web app setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>🎉 Welcome to TriviCoin 🎉</h1>
+        <p>Miragaming.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
